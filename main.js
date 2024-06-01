ef1a1b4ba0e6074656a27264ce5ff4550b940724
ditam <ditam@users.noreply.github.com>
2024-06-01 15:08:48 +0200

    add static grid with pedestrians

--- main.js
+++ main.js
@@ -1,38 +1,152 @@
+// --- constants
+const WIDTH = 1000; // 1280;
+const HEIGHT = 600; // 720;
+const ROAD_WIDTH = 30;
 
-const WIDTH = 800;
-const HEIGHT = 500;
+const PEOPLE_COUNT = 20;
+const HOUSE_CHANCE = 0.62;
 
-let ctx;
-
-let xPosition = 0;
-
-function drawFrame(timestamp) {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-
-  ctx.beginPath();
-  ctx.moveTo(225, 300);
-  ctx.lineTo(325, 450);
-  ctx.lineTo(625,  50);
-  ctx.stroke();
-
-  xPosition = (timestamp/3) % WIDTH;
-  ctx.fillRect(xPosition, HEIGHT/2-10, 20, 20);
-
-  requestAnimationFrame(drawFrame);
+// --- utils
+function getRandomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function getRandomInt(min, max) { // min and max included
+  if (typeof max === 'undefined') {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function deepCopy(o) {
+  return JSON.parse(JSON.stringify(o));
 }
 
+// --- globals
+let container;
+
+// --- rest of the stuff
+function drawRoads() {
+  // horizontals
+  const r1 = $('<div>').addClass('road').appendTo(container);
+  r1.css({
+    top: HEIGHT/3 + 'px',
+    left: 0,
+    width: WIDTH,
+    height: ROAD_WIDTH
+  });
+
+  const r2 = $('<div>').addClass('road').appendTo(container);
+  r2.css({
+    top: HEIGHT/3 * 2 + 'px',
+    left: 0,
+    width: WIDTH,
+    height: ROAD_WIDTH
+  });
+
+  // verticals
+  const r3 = $('<div>').addClass('road').appendTo(container);
+  r3.css({
+    top: 0,
+    left: (WIDTH / 4 * 1) + 'px',
+    width: ROAD_WIDTH,
+    height: HEIGHT
+  });
+  const r4 = $('<div>').addClass('road').appendTo(container);
+  r4.css({
+    top: 0,
+    left: (WIDTH / 4 * 2) + 'px',
+    width: ROAD_WIDTH,
+    height: HEIGHT
+  });
+  const r5 = $('<div>').addClass('road').appendTo(container);
+  r5.css({
+    top: 0,
+    left: (WIDTH / 4 * 3) + 'px',
+    width: ROAD_WIDTH,
+    height: HEIGHT
+  });
+}
+
+let houseGrid;
+function generateHouses() {
+  houseGrid = [];
+  for (let j=0; j<3; j++) {
+    for (let i=0; i<4; i++) {
+      const group = {
+        groupPosition: { x: i, y: j },
+        houses: [
+          [Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE],
+          [Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE],
+          [Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE, Math.random() < HOUSE_CHANCE],
+        ]
+      };
+      houseGrid.push(group);
+    }
+  }
+  console.log('generated houses:', houseGrid);
+}
+
+function start() {
+  drawRoads();
+
+  // draw random people
+  const people = [];
+  const roadXs = [WIDTH/4, WIDTH/4*2, WIDTH/4*3];
+  const roadYs = [HEIGHT/3, HEIGHT/3*2];
+  for (let i=0; i<PEOPLE_COUNT; i++) {
+    let isVertical = Math.random() < 0.5;
+    if (isVertical) {
+      people.push({
+        dir: 'RIGHT',
+        x: getRandomItem(roadXs),
+        y: getRandomInt(0, HEIGHT),
+      });
+    }
+    else {
+      people.push({
+        dir: 'DOWN',
+        x: getRandomInt(0, HEIGHT),
+        y: getRandomItem(roadYs),
+      });
+    }
+  }
+  people.forEach(p=>{
+    $('<div>').addClass('person').css({
+      top: p.y,
+      left: p.x
+    }).appendTo(container);
+  });
+
+  // generate houses
+  generateHouses();
+  // draw houses
+  const SIZE = 50; // should be bigger than CSS .house size
+  const GRID_OFFSET = 50; // move the grid X and Y to not overlap roads
+  houseGrid.forEach(group => {
+    const x0 = GRID_OFFSET + group.groupPosition.x * WIDTH/4;
+    const y0 = GRID_OFFSET + group.groupPosition.y * HEIGHT/3;
+    group.houses.forEach((row, rowIndex) => {
+      row.forEach((h, index) => {
+        if (h) {
+          $('<div>').addClass('house').css({
+            top: y0 + rowIndex * SIZE,
+            left: x0 + index * SIZE
+          }).appendTo(container);
+        }
+      });
+    });
+  });
+};
+
 $(document).ready(function() {
-  console.log('Hello Canvas!');
+  console.log('Hello Meteor!');
 
-  const canvas = document.getElementById('main-canvas');
-  $(canvas).attr('height', HEIGHT);
-  $(canvas).attr('width', WIDTH);
+  container = $('#main-container');
 
-  ctx = canvas.getContext('2d');
+  container.css({
+    width: WIDTH,
+    height: HEIGHT
+  });
 
-  ctx.fillStyle = '#00FF00';
-  ctx.strokeStyle = 'green';
-  ctx.lineWidth = 5;
-
-  drawFrame();
+  start();
 });
